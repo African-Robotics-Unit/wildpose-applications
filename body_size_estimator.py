00d5db6afeeb50b36b528f61dcc1f9356a5cbd10
DenDen047 <sh.mn.nat@gmail.com>
2023-07-12 03:20:19 +0200

    feat: success to load the pcd files with open3d

--- body_size_estimator.py
+++ body_size_estimator.py
@@ -1,12 +1,42 @@
+import os
+import glob
 from pprint import pprint
 
-from utils.config import load_config_file
+from utils.file_loader import load_config_file, load_pcd
+from utils.format_conversion import get_timestamp_from_pcd_fpath
 
 
 def main():
+    # args
     config_fpath = 'config.hjson'
+    pcd_mode = 'open3d'
+
+    # load the config file
     config = load_config_file(config_fpath)
-    pprint(config)
+
+    # get file paths of textured point cloud
+    all_pcd_fpaths = sorted(glob.glob(
+        os.path.join(config['textured_pcd_folder'], '*.pcd')))
+    pcd_fpaths = sorted([f for f in all_pcd_fpaths if '_mask.pcd' not in f])
+    pcd_mask_fpaths = sorted([f for f in all_pcd_fpaths if '_mask.pcd' in f])
+    timestamps = sorted([
+        get_timestamp_from_pcd_fpath(f)
+        for f in pcd_fpaths
+    ])
+    assert len(pcd_fpaths) == len(pcd_mask_fpaths) == len(timestamps)
+
+    for (pcd_fpath, pcd_mask_fpath, timestamp) in zip(
+        pcd_fpaths, pcd_mask_fpaths, timestamps
+    ):
+        # load pcd files
+        pcd = load_pcd(pcd_fpath, mode=pcd_mode)
+        pcd_mask = load_pcd(pcd_mask_fpath, mode=pcd_mode)
+
+        # pick the target dots up
+        pass
+
+        # show the PCD in the viewer
+        pass
 
 
 if __name__ == '__main__':
--- utils/config.py
+++
@@ -1,8 +0,0 @@
-import hjson
-
-
-def load_config_file(fpath: str):
-    config = None
-    with open(fpath, mode='r') as f:
-        config = hjson.loads(f.read())
-    return config
