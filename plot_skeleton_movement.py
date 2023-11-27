bd383bf8a6cb9972549b64e8f0a46ddb73e179ca
DenDen047 <sh.mn.nat@gmail.com>
2023-11-27 09:48:06 +0900

    chore: daily update

--- plot_skeleton_movement.py
+++ plot_skeleton_movement.py
@@ -31,27 +31,31 @@ CONFIG = {
     "textured_pcd_dir": "data/lion_walk/textured_pcds",
 }
 IMG_WIDTH, IMG_HEIGHT = 1280, 720
-XLIM = (-1, 0.2)
-YLIM = (-0.4, 0.6)
-ZLIM = (22, 24)
+XLIM = (-1, 1)
+YLIM = (-1, 1)
+ZLIM = (42, 45)
 
-JOINTS = {
-    'nose': [290, 315],
-    'r_eye': [None, None],
-    'l_eye': [338, 272],
-    'r_shoulder': [None, None],
-    'r_elbow': [535, 457],
-    'r_wrist': [551, 590],
-    'r_hip': [None, None],
-    'r_knee': [None, None],
-    'r_ankle': [718, 552],
-    'l_shoulder': [452, 373],
-    'l_elbow': [487, 451],
-    'l_wrist': [498, 587],
-    'l_hip': [630, 358],
-    'l_knee': [623, 465],
-    'l_ankle': [653, 555],
-}
+JOINTS = [
+    {
+        'rgb_filename': '659835_1670481560_458554312.jpeg',
+        'lidar_filename': 'livox_frame_1670481560_459611816.pcd',
+        'nose': [473, 215],
+        'r_eye': [436, 158],
+        'l_eye': [489, 149],
+        'r_shoulder': [None, None],
+        'r_elbow': [None, None],
+        'r_wrist': [495, 437],
+        'r_hip': [None, None],
+        'r_knee': [None, None],
+        'r_ankle': [None, None],
+        'l_shoulder': [593, 192],
+        'l_elbow': [595, 337],
+        'l_wrist': [596, 481],
+        'l_hip': [769, 377],
+        'l_knee': [663, 377],
+        'l_ankle': [715, 475],
+    }
+]
 
 
 def lidar2cam_projection(pcd, extrinsic):
@@ -156,78 +160,81 @@ def main():
     output_dir = CONFIG['textured_pcd_dir']
 
     lidar_list, rgb_list = sync_lidar_and_rgb(lidar_dir, rgb_dir)
-    fx, fy, cx, cy, rot_mat, translation = load_camera_parameters(calib_fpath)
-
-    # load the texture image
-    rgb_img = load_rgb_img(texture_img_fpath)
-
-    # accumulate all the point cloud
-    accumulated_pcd_in_lidar = None
-    for pcd_fpath in lidar_list:
-        pcd_in_lidar = o3d.io.read_point_cloud(pcd_fpath)
-        pcd_points = np.asarray(pcd_in_lidar.points)  # [N, 3]
-
-        if accumulated_pcd_in_lidar is None:
-            accumulated_pcd_in_lidar = pcd_points
-        else:
-            accumulated_pcd_in_lidar = np.vstack((accumulated_pcd_in_lidar, pcd_points))
 
     # load the camera parameters
+    fx, fy, cx, cy, rot_mat, translation = load_camera_parameters(calib_fpath)
     intrinsic = make_intrinsic_mat(fx, fy, cx, cy)
     extrinsic = make_extrinsic_mat(rot_mat, translation)
 
-    # project the point cloud to camera and its image sensor
-    pcd_in_cam = lidar2cam_projection(accumulated_pcd_in_lidar, extrinsic)
-    pcd_in_img = cam2image_projection(pcd_in_cam, intrinsic)
-    pcd_in_cam = pcd_in_cam.T[:, :-1]
-    pcd_in_img = pcd_in_img.T[:, :-1]
+    for joint_info in JOINTS:
+        # load the texture image
+        img_fpath = os.path.join(rgb_dir, joint_info['rgb_filename'])
+        rgb_img = load_rgb_img(img_fpath)
 
-    pcd_colors, valid_mask_save = extract_rgb_from_image_pure(
-        pcd_in_img, rgb_img, width=IMG_WIDTH, height=IMG_HEIGHT)
-    pcd_with_rgb = np.concatenate([pcd_in_cam, pcd_colors], 1)
-    pcd_with_rgb = pcd_with_rgb[valid_mask_save]  # [N, 6]
-    textured_pcd = o3d.geometry.PointCloud()
-    textured_pcd.points = o3d.utility.Vector3dVector(
-        pcd_with_rgb[:, :3])
-    textured_pcd.colors = o3d.utility.Vector3dVector(
-        pcd_with_rgb[:, 3:])
-    # save the coloured point cloud
-    o3d.io.write_point_cloud(
-        os.path.join(output_dir, 'coloured_accumulation.pcd'),
-        textured_pcd)
+        # load the pcd
+        pcd_fpath = os.path.join(lidar_dir, joint_info['lidar_filename'])
+        pcd_in_lidar = o3d.io.read_point_cloud(pcd_fpath)
+        pcd_points = np.asarray(pcd_in_lidar.points)  # [N, 3]
 
-    # load data
-    points = np.array(textured_pcd.points)  # [N, 3]
-    colors = np.array(textured_pcd.colors)  # [N, 3]
+        # project the point cloud to camera and its image sensor
+        pcd_in_cam = lidar2cam_projection(pcd_points, extrinsic)
+        pcd_in_img = cam2image_projection(pcd_in_cam, intrinsic)
+        pcd_in_cam = pcd_in_cam.T[:, :-1]
+        pcd_in_img = pcd_in_img.T[:, :-1]
 
-    # make the mask
-    mask = (
-        (XLIM[0] < points[:, 0]) & (points[:, 0] < XLIM[1]) &
-        (YLIM[0] < points[:, 1]) & (points[:, 1] < YLIM[1]) &
-        (ZLIM[0] < points[:, 2]) & (points[:, 2] < ZLIM[1])
-    )
-    masked_points = points[mask]
-    masked_colors = colors[mask]
+        pcd_colors, valid_mask_save = extract_rgb_from_image_pure(
+            pcd_in_img, rgb_img, width=IMG_WIDTH, height=IMG_HEIGHT)
+        pcd_with_rgb = np.concatenate([pcd_in_cam, pcd_colors], 1)
+        pcd_with_rgb = pcd_with_rgb[valid_mask_save]  # [N, 6]
+        textured_pcd = o3d.geometry.PointCloud()
+        textured_pcd.points = o3d.utility.Vector3dVector(
+            pcd_with_rgb[:, :3])
+        textured_pcd.colors = o3d.utility.Vector3dVector(
+            pcd_with_rgb[:, 3:])
 
-    # plot the data
-    ax = plt.figure().add_subplot(projection='3d')
-    ax.scatter(
-        masked_points[:, 0], masked_points[:, 1], masked_points[:, 2],
-        c=masked_colors, s=1)  # s is the size of the points
-    for joint_name, joint_2d in JOINTS.items():
-        if joint_2d[0] is not None and joint_2d[1] is not None:
-            _, idx = get_3d_from_2d_point(pcd_in_img, joint_2d)
-            joint_3d = pcd_in_cam[idx, :]
-            ax.scatter(
-                joint_3d[0], joint_3d[1], joint_3d[2],
-                color='r', s=20)
-            print(f'{joint_name}: {joint_3d}')
-    equal_3d_aspect(ax=ax)
-    ax.set_xlabel('x (m)')
-    ax.set_ylabel('y (m)')
-    ax.set_zlabel('Depth (m)')
+        # save the coloured point cloud
+        o3d.io.write_point_cloud(
+            os.path.join(output_dir, 'coloured_accumulation.pcd'),
+            textured_pcd)
 
-    plt.show()
+        # load data
+        points = np.array(textured_pcd.points)  # [N, 3]
+        colors = np.array(textured_pcd.colors)  # [N, 3]
+
+        # make the mask
+        mask = (
+            (XLIM[0] < points[:, 0]) & (points[:, 0] < XLIM[1]) &
+            (YLIM[0] < points[:, 1]) & (points[:, 1] < YLIM[1]) &
+            (ZLIM[0] < points[:, 2]) & (points[:, 2] < ZLIM[1])
+        )
+        masked_points = points[mask]
+        masked_colors = colors[mask]
+
+        # plot the data
+        ax = plt.figure().add_subplot(projection='3d')
+        ax.scatter(
+            masked_points[:, 0], masked_points[:, 1], masked_points[:, 2],
+            c=masked_colors, s=1)  # s is the size of the points
+        for joint_name, joint_2d in joint_info.items():
+            if (
+                isinstance(joint_2d, list) and
+                joint_2d[0] is not None and joint_2d[1] is not None
+            ):
+                _, idx = get_3d_from_2d_point(pcd_in_img, joint_2d)
+                joint_3d = pcd_in_cam[idx, :]
+                ax.scatter(
+                    joint_3d[0], joint_3d[1], joint_3d[2],
+                    color='r', s=20)
+                print(f'{joint_name}: {joint_3d}')
+        equal_3d_aspect(ax=ax)
+        ax.set_xlabel('x (m)')
+        ax.set_ylabel('y (m)')
+        ax.set_zlabel('Depth (m)')
+
+        plt.show()
+
+
+        break
 
 
 if __name__ == '__main__':
